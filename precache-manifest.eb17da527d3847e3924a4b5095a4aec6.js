219052457b7b4087304e787533b2ed9962e4b3f1
Inaki <imckearney01@qub.ac.uk>
2020-06-05 19:30:07 +0100

    Updates

---
+++ precache-manifest.eb17da527d3847e3924a4b5095a4aec6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc42bd068e82331deaf44c44dbd4e5d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "363fe37eb2c04069833d",
+    "url": "/weather-app/static/css/main.62ecec88.chunk.css"
+  },
+  {
+    "revision": "1faad7f255ee893c55bc",
+    "url": "/weather-app/static/js/2.a4fb13ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.a4fb13ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363fe37eb2c04069833d",
+    "url": "/weather-app/static/js/main.54fb482f.chunk.js"
+  },
+  {
+    "revision": "490dbced286fae99269b",
+    "url": "/weather-app/static/js/runtime-main.74b9901d.js"
+  },
+  {
+    "revision": "83dd935016b0eb9b4f88a133e0822b2c",
+    "url": "/weather-app/static/media/clouds.83dd9350.jpg"
+  },
+  {
+    "revision": "018c9b792fd9f0bb02e108119a948379",
+    "url": "/weather-app/static/media/rain2.018c9b79.jpg"
+  },
+  {
+    "revision": "352d093dc21690e4a8320cf5f0c88e27",
+    "url": "/weather-app/static/media/sun.352d093d.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-app/static/media/warm.635ed87d.jpg"
+  }
+]);
